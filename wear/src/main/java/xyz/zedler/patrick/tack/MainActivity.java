240c692ea733e11586aafe5ba8aa29e604a665e1
Patrick Zedler <patrick@zedler.xyz>
2020-08-28 22:30:14 +0200

    Remove data binding in wear

--- wear/src/main/java/xyz/zedler/patrick/tack/MainActivity.java
+++ wear/src/main/java/xyz/zedler/patrick/tack/MainActivity.java
@@ -39,7 +39,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import xyz.zedler.patrick.tack.databinding.ActivityMainBinding;
 import xyz.zedler.patrick.tack.util.Constants;
 import xyz.zedler.patrick.tack.view.BpmPickerView;
 
@@ -49,7 +48,6 @@ public class MainActivity extends WearableActivity
     private final static String TAG = MainActivity.class.getSimpleName();
     private final static boolean DEBUG = false;
 
-    private ActivityMainBinding binding;
     private SharedPreferences sharedPrefs;
     private Vibrator vibrator;
     private int bpm, emphasis, emphasisIndex, rotaryFactorIndex = 0, rotatedPrev = 0;
@@ -83,7 +81,6 @@ public class MainActivity extends WearableActivity
         interval = sharedPrefs.getLong(Constants.PREF.INTERVAL, 500);
         bpm = toBpm(interval);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(hidePicker ? R.layout.activity_main : R.layout.activity_main_picker);
 
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
