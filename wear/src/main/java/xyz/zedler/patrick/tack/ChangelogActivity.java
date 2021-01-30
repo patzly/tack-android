6d27f70ed21bcae7ea6b5ccae04a100aa2f156e0
Patrick Zedler <patrick@zedler.xyz>
2021-01-30 04:13:32 +0100

    Implement proper bullet list in changelog

--- wear/src/main/java/xyz/zedler/patrick/tack/ChangelogActivity.java
+++ wear/src/main/java/xyz/zedler/patrick/tack/ChangelogActivity.java
@@ -1,7 +1,6 @@
 package xyz.zedler.patrick.tack;
 
 import android.os.Bundle;
-import android.text.Html;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
@@ -9,6 +8,7 @@ import androidx.fragment.app.FragmentActivity;
 import java.util.Locale;
 
 import xyz.zedler.patrick.tack.databinding.ActivityChangelogBinding;
+import xyz.zedler.patrick.tack.util.BulletUtil;
 import xyz.zedler.patrick.tack.util.ResUtil;
 
 public class ChangelogActivity extends FragmentActivity {
@@ -29,7 +29,15 @@ public class ChangelogActivity extends FragmentActivity {
         localized = localized != null ? localized : ResUtil.readFromFile(this, file);
 
         binding.textChangelog.setText(
-                Html.fromHtml(getAsHtml(localized)), TextView.BufferType.SPANNABLE
+                BulletUtil.makeBulletList(
+                        this,
+                        6,
+                        2,
+                        "- ",
+                        localized,
+                        getResources().getStringArray(R.array.changelog_highlights)
+                ),
+                TextView.BufferType.SPANNABLE
         );
     }
 
@@ -37,20 +45,5 @@ public class ChangelogActivity extends FragmentActivity {
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
-    }
-
-    private String getAsHtml(String text) {
-        text = text.replaceAll("\n", "<br/>");
-        String textNew = getString(R.string.changelog_new);
-        text = text.replaceAll(textNew, getBold(textNew));
-        String textImproved = getString(R.string.changelog_improved);
-        text = text.replaceAll(textImproved, getBold(textImproved));
-        String textFixed = getString(R.string.changelog_fixed);
-        text = text.replaceAll(textFixed, getBold(textFixed));
-        return text;
-    }
-
-    private String getBold(String text) {
-        return "<b>" + text + "</b>";
     }
 }
