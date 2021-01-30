58e2f3c8dc7551bf24825fec5138524a67610563
Patrick Zedler <patrick@zedler.xyz>
2021-01-30 17:53:00 +0100

    Implement splash screen workaround

---
+++ wear/src/main/java/xyz/zedler/patrick/tack/SplashActivity.java
@@ -0,0 +1,17 @@
+package xyz.zedler.patrick.tack;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class SplashActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
