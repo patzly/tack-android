96ba6df099326bbd87c1167b459a62d72f74e216
Patrick Zedler <patrick@zedler.xyz>
2021-01-30 17:12:26 +0100

    Remove localized changelog

--- wear/src/main/java/xyz/zedler/patrick/tack/ChangelogActivity.java
+++ wear/src/main/java/xyz/zedler/patrick/tack/ChangelogActivity.java
@@ -5,8 +5,6 @@ import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 
-import java.util.Locale;
-
 import xyz.zedler.patrick.tack.databinding.ActivityChangelogBinding;
 import xyz.zedler.patrick.tack.util.BulletUtil;
 import xyz.zedler.patrick.tack.util.ResUtil;
@@ -22,19 +20,13 @@ public class ChangelogActivity extends FragmentActivity {
         binding = ActivityChangelogBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        String file = "changelog";
-        String fileLocalized = file + "-" + Locale.getDefault().getLanguage();
-
-        String localized = ResUtil.readFromFile(this, fileLocalized);
-        localized = localized != null ? localized : ResUtil.readFromFile(this, file);
-
         binding.textChangelog.setText(
                 BulletUtil.makeBulletList(
                         this,
                         6,
                         2,
                         "- ",
-                        localized,
+                        ResUtil.readFromFile(this, "changelog"),
                         getResources().getStringArray(R.array.changelog_highlights)
                 ),
                 TextView.BufferType.SPANNABLE
