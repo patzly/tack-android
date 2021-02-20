4c63af55f4601edaae9a517400ab7120a5516451
Patrick Zedler <patrick@zedler.xyz>
2021-02-20 22:59:26 +0100

    Don't show splash screen on SDK lower than 23

--- app/src/main/java/xyz/zedler/patrick/tack/util/UnitUtil.java
+++ app/src/main/java/xyz/zedler/patrick/tack/util/UnitUtil.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.WindowInsets;
+import android.view.WindowManager;
 import android.view.WindowMetrics;
 
 public class UnitUtil {
@@ -27,6 +28,7 @@ public class UnitUtil {
         );
     }
 
+    @Deprecated
     public static int getDisplayHeight(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             WindowMetrics windowMetrics = activity.getWindowManager().getCurrentWindowMetrics();
@@ -37,6 +39,20 @@ public class UnitUtil {
         } else {
             DisplayMetrics displayMetrics = new DisplayMetrics();
             activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            return displayMetrics.heightPixels;
+        }
+    }
+
+    public static int getDisplayHeight(WindowManager windowManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowMetrics windowMetrics = windowManager.getCurrentWindowMetrics();
+            Insets insets = windowMetrics.getWindowInsets().getInsetsIgnoringVisibility(
+                    WindowInsets.Type.systemBars()
+            );
+            return windowMetrics.getBounds().height() - insets.top - insets.bottom;
+        } else {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
             return displayMetrics.heightPixels;
         }
     }
