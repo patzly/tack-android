8f85f0ee3c6b14f2193f55b4d57e91d38c34d1c3
patzly <patrick.zedler.music@gmail.com>
2021-10-18 03:24:14 +0200

    Implement Android 12 splash screen

--- app/src/main/java/xyz/zedler/patrick/tack/activity/SplashActivity.java
+++ app/src/main/java/xyz/zedler/patrick/tack/activity/SplashActivity.java
@@ -1,8 +1,7 @@
 package xyz.zedler.patrick.tack.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.AnimatedVectorDrawable;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,41 +11,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import xyz.zedler.patrick.tack.R;
 import xyz.zedler.patrick.tack.behavior.SystemBarBehavior;
+import xyz.zedler.patrick.tack.util.ViewUtil;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
+  @Override
   public void onCreate(Bundle bundle) {
     super.onCreate(bundle);
 
     new SystemBarBehavior(this).setUp();
 
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-      startMainActivity();
-      return;
-    }
-
-    LayerDrawable splashContent = (LayerDrawable) ResourcesCompat.getDrawable(
-        getResources(), R.drawable.splash_content, null
-    );
-
-    getWindow().setBackgroundDrawable(splashContent);
-
-    try {
-      assert splashContent != null;
-      Drawable splashLogo = splashContent.findDrawableByLayerId(R.id.splash_logo);
-      AnimatedVectorDrawable logo = (AnimatedVectorDrawable) splashLogo;
-      logo.start();
-      new Handler(Looper.getMainLooper()).postDelayed(
-          this::startMainActivity, 1000
+    if (Build.VERSION.SDK_INT >= 31) {
+      startSettingsActivity(false);
+    } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+      LayerDrawable splashContent = (LayerDrawable) ResourcesCompat.getDrawable(
+          getResources(), R.drawable.splash_content, null
       );
-    } catch (Exception e) {
-      startMainActivity();
+      getWindow().setBackgroundDrawable(splashContent);
+      try {
+        assert splashContent != null;
+        ViewUtil.startIcon(splashContent.findDrawableByLayerId(R.id.splash_logo));
+        new Handler(Looper.getMainLooper()).postDelayed(
+            () -> startSettingsActivity(true), 1000
+        );
+      } catch (Exception e) {
+        startSettingsActivity(true);
+      }
+    } else {
+      startSettingsActivity(true);
     }
   }
 
-  private void startMainActivity() {
+  private void startSettingsActivity(boolean fadeOut) {
     startActivity(new Intent(this, MainActivity.class));
-    overridePendingTransition(0, R.anim.fade_out);
+    overridePendingTransition(0, fadeOut ? R.anim.fade_out : 0);
     finish();
   }
 }
