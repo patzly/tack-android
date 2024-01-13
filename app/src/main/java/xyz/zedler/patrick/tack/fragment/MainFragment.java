f782ac4d5f8f623bb0da1fc5bfe7f73d7af5c58c
Patrick Zedler <mail@patrickzedler.com>
2024-01-13 20:04:01 +0100

    Add GitHub assets

--- app/src/main/java/xyz/zedler/patrick/tack/fragment/MainFragment.java
+++ app/src/main/java/xyz/zedler/patrick/tack/fragment/MainFragment.java
@@ -446,7 +446,6 @@ public class MainFragment extends BaseFragment
     ViewUtil.setTooltipText(binding.buttonMainRemoveSubdivision, R.string.action_remove_sub);
     ViewUtil.setTooltipText(binding.buttonMainOptions, R.string.title_options);
     ViewUtil.setTooltipText(binding.buttonMainBeatMode, R.string.action_beat_mode);
-    ViewUtil.setTooltipText(binding.fabMainPlayStop, R.string.action_play_stop);
 
     ViewUtil.setOnClickListeners(
         this,
