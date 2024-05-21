5f2ea142aeab484ed2c1ab9d1db46cfa659c9e57
Patrick Zedler <mail@patrickzedler.com>
2024-05-21 20:46:26 +0200

    Downgrade Kotlin version due to incompatible Compose compiler stable version

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -55,7 +55,7 @@ android {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.1"
+    kotlinCompilerExtensionVersion = "1.5.14"
   }
   packaging {
     resources {
