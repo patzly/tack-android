d9ed98cef5cd25a167972821e4c4a38cd3319de2
Patrick Zedler <mail@patrickzedler.com>
2024-06-14 22:47:45 +0200

    Adjust Java versions

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -47,7 +47,7 @@ android {
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
     jvmTarget = "11"
