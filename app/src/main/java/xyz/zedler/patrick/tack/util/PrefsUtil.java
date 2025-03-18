edd449f96f5fa621f50f4b2dcc981ac463ab68cf
Patrick Zedler <mail@patrickzedler.com>
2025-03-18 03:17:40 +0100

    Log bookmarks migration

--- app/src/main/java/xyz/zedler/patrick/tack/util/PrefsUtil.java
+++ app/src/main/java/xyz/zedler/patrick/tack/util/PrefsUtil.java
@@ -96,32 +96,31 @@ public class PrefsUtil {
       // from bookmarks to songs
       Set<String> bookmarks = sharedPrefs.getStringSet(BOOKMARKS, Set.of());
       if (!bookmarks.isEmpty()) {
-        new Handler(Looper.getMainLooper()).post(() -> {
-          List<Song> songsToAdd = new LinkedList<>();
-          List<Part> partsToAdd = new LinkedList<>();
-          sharedPrefs.edit().remove(BOOKMARKS).apply();
-          for (String bookmark : bookmarks) {
-            try {
-              int tempo = Integer.parseInt(bookmark);
-              String songName = context.getString(R.string.label_bpm_value, tempo);
-              Song song = new Song(songName);
-              songsToAdd.add(song);
-              MetronomeConfig config = new MetronomeConfig();
-              config.setTempo(tempo);
-              partsToAdd.add(new Part(null, song.getId(), config));
-            } catch (NumberFormatException e) {
-              Log.e(TAG, "migrateBookmarks: bookmark to tempo: ", e);
-            }
+        List<Song> songsToAdd = new LinkedList<>();
+        List<Part> partsToAdd = new LinkedList<>();
+        sharedPrefs.edit().remove(BOOKMARKS).apply();
+        for (String bookmark : bookmarks) {
+          try {
+            int tempo = Integer.parseInt(bookmark);
+            String songName = context.getString(R.string.label_bpm_value, tempo);
+            Song song = new Song(songName);
+            songsToAdd.add(song);
+            MetronomeConfig config = new MetronomeConfig();
+            config.setTempo(tempo);
+            partsToAdd.add(new Part(null, song.getId(), config));
+            Log.i(TAG, "migrateBookmarks: added " + song + " for " + bookmark);
+          } catch (NumberFormatException e) {
+            Log.e(TAG, "migrateBookmarks: bookmark to tempo: ", e);
           }
-          executorService.execute(() -> {
-            SongDatabase db = SongDatabase.getInstance(context.getApplicationContext());
-            db.songDao().insertSongs(songsToAdd);
-            db.songDao().insertParts(partsToAdd);
-          });
-          // Remove deprecated shortcuts
-          ShortcutUtil shortcutUtil = new ShortcutUtil(context);
-          shortcutUtil.removeAllShortcuts();
+        }
+        executorService.execute(() -> {
+          SongDatabase db = SongDatabase.getInstance(context.getApplicationContext());
+          db.songDao().insertSongs(songsToAdd);
+          db.songDao().insertParts(partsToAdd);
         });
+        // Remove deprecated shortcuts
+        ShortcutUtil shortcutUtil = new ShortcutUtil(context);
+        shortcutUtil.removeAllShortcuts();
       }
     }
   }
