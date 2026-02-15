f32b40012e406d2013146cdda2bcc44e4af79f73
Patrick Zedler <mail@patrickzedler.com>
2026-02-15 01:43:31 +0100

    Remove Kotlin plugin for built-in Kotlin

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -103,7 +103,6 @@ configure<ApplicationExtension> {
   externalNativeBuild {
     cmake {
       path = file("src/main/cpp/CMakeLists.txt")
-      version = "4.0.2"
     }
   }
   ndkVersion = "29.0.14206865"
